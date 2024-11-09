f968adcab7c5fc18299178d0cbde0251fed3e792
itrytolearn <manu_cercelaru@yahoo.com>
2024-11-09 10:57:30 +0200

    Fix money comparison issue rails 5

--- app/controllers/spree/admin/prices_controller.rb
+++ app/controllers/spree/admin/prices_controller.rb
@@ -9,8 +9,10 @@ module Spree
           next unless variant
           supported_currencies.each do |currency|
             price = variant.price_in(currency.iso_code)
-            price.price = (prices[currency.iso_code].blank? ? nil : prices[currency.iso_code].to_money)
-            price.save! if price.new_record? && price.price || !price.new_record? && price.changed?
+            price.price = prices[currency.iso_code].presence
+            if price.price.present?
+              price.save! if price.new_record? || price.changed?
+            end
           end
         end
         flash[:success] = Spree.t('notice.prices_saved')
